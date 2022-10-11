a50d6ec38c3bb19f0886be4098a92a1c66a70d09
ththanh <hoaithanhtrinh2002@gmail.com>
2022-10-11 15:53:43 +0700

    bill

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+var KM1_UBERX = 8000;
+var KMDUOI20_UBERX = 12000;
+var KMTREN20_UBERX = 10000;
+var TGCHO_UBERX = 2000;
+var KM1_UBER_SUV = 9000;
+var KMDUOI20_UBER_SUV = 14000;
+var KMTREN20_UBER_SUV = 12000;
+var TGCHO_UBER_SUV = 3000;
+var KM1_UBER_BLACK = 10000;
+var KMDUOI20_UBER_BLACK = 16000;
+var KMTREN20_UBER_BLACK = 14000;
+var TGCHO_UBER_BLACK = 4000;
 function getElement(ele){
     return document.getElementById(ele);
 }
@@ -24,24 +36,13 @@ function TinhTien(){
     laySoKM = parseFloat(laySoKM);
     var layThoiGianCho  = document.getElementById('thoiGianCho').value;
     layThoiGianCho = parseFloat(layThoiGianCho);
-    console.log(laySoKM);
-    console.log(layThoiGianCho);    
+    // console.log(laySoKM);
+    // console.log(layThoiGianCho);    
     var divThanhTien  = document.getElementById('divThanhTien');
     divThanhTien.style.display = 'block';
     var spanTien = document.getElementById('xuatTien');
     var loaiXe = LayLoaiXe();
-    var KM1_UBERX = 8000;
-    var KMDUOI20_UBERX = 12000;
-    var KMTREN20_UBERX = 10000;
-    var TGCHO_UBERX = 2000;
-    var KM1_UBER_SUV = 9000;
-    var KMDUOI20_UBER_SUV = 14000;
-    var KMTREN20_UBER_SUV = 12000;
-    var TGCHO_UBER_SUV = 3000;
-    var KM1_UBER_BLACK = 10000;
-    var KMDUOI20_UBER_BLACK = 16000;
-    var KMTREN20_UBER_BLACK = 14000;
-    var TGCHO_UBER_BLACK = 4000;
+    
     var thanhTien = 0;
     switch (loaiXe){
         case 'uberX':
@@ -79,9 +80,46 @@ function TinhTien(){
             }
             break;
         default:   
+            var divThanhTien  = document.getElementById('divThanhTien');
+            divThanhTien.style.display = 'none';
             alert('Vui lòng chọn loại xe');
             break;
     }
    
      spanTien.innerHTML = thanhTien;
+}
+function XuatUberX(){
+    var tenUber = document.getElementById('ten-uber');
+    tenUber.innerHTML = 'UberX';
+    var laySoKM  = document.getElementById('soKM').value;
+    laySoKM = parseFloat(laySoKM);
+    var suDung = document.getElementById('su-dung');
+    if (laySoKM<=1){
+        suDung.innerHTML = laySoKM+' km';
+    } 
+    if(laySoKM>1&&laySoKM<=20){
+        suDung.innerHTML = 1+' km';
+        suDung.innerHTML = (laySoKM-1) +' km';
+    }
+}
+function XuatTotal(){
+    var tongTien = TinhTien();
+    var total = document.getElementById('tong-tien');
+    total.innerHTML = tongTien;
+}
+function XuatHoaDon(){
+    var hoaDon = document.getElementById('hoa-don');
+    hoaDon.style.display = 'block';
+    var loaiXe = LayLoaiXe();
+    switch(loaiXe)
+    {
+        case 'uberX':
+            XuatUberX();
+            break;
+    }
+    XuatTotal();
+}
+function TatHoaDon(){
+    var hoaDon = document.getElementById('hoa-don');
+    hoaDon.style.display = 'none';
 }
